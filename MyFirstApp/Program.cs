7121f42ba82a393bb1ccba140e3a3e12d293e0c2
frunt <frunt@DESKTOP-02KKATA>
2019-11-26 20:48:46 +0300

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            //mySuperApplication - lower camel case
+            /*
+            MySuperApplication - upper camel case
+
+            MY_SUPER_APPLICATION - upper snake case
+            */
+
+            Console.Write("Hello");
+            Console.WriteLine(" world"); //
+            Console.Write("!");
+
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
